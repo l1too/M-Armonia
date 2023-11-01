504a38a8c41510cd4965ecdbe79cf52f39266ab2
Gabriel Quiroga <gabrielnahuel.quiroga@gmail.com>
2023-10-31 22:42:43 -0300

    creando proyecto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+// import { Routes, Route } from 'react-router-dom' 
+import NavBar from './components/NavBar'
+
+function App() {
+
+
+  return (
+    
+    <>
+          <NavBar/>
+    </>
+   
+  )
+}
+
+export default App
